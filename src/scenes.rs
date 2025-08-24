b43504ffda3fcf7e5ec0c27d4c0fdc2c830d3844
Cyril Flaig <code@flaig.ch>
2025-08-24 17:55:33 +0200

    Use a shape enum instead of a trait

--- src/scenes.rs
+++ src/scenes.rs
@@ -1,10 +1,10 @@
 use glam::{Mat4, Vec3};
 
-use crate::shape::{Cone, Cylinder, Plane, Shape, Sphere, TransformedShape, UnitBox};
+use crate::shape::Shape;
 use crate::types::{Camera, Material, Transform};
 
 // Scene builders
-pub fn make_default_scene() -> (Camera, Vec3, Vec<Box<dyn Shape>>) {
+pub fn make_default_scene() -> (Camera, Vec3, Vec<Shape>) {
     let camera = Camera::new(
         Vec3::new(0.0, -5.0, -1.25),
         Vec3::new(0.0, 0.0, 0.0),
@@ -29,19 +29,19 @@ pub fn make_default_scene() -> (Camera, Vec3, Vec<Box<dyn Shape>>) {
         specular_coef: 0.0,
     };
 
-    let shapes: Vec<Box<dyn Shape>> = vec![
-        Box::new(Cone { material: red }),
-        Box::new(Plane {
+    let shapes: Vec<Shape> = vec![
+        Shape::Cone { material: red },
+        Shape::Plane {
             normal: Vec3::new(0.0, 0.0, 1.0),
             d: -2.0,
             material: blue,
-        }),
+        },
     ];
     (camera, light, shapes)
 }
 
 #[allow(dead_code)]
-pub fn make_scene_with_eight_boxes() -> (Camera, Vec3, Vec<Box<dyn Shape>>) {
+pub fn make_scene_with_eight_boxes() -> (Camera, Vec3, Vec<Shape>) {
     let camera = Camera::new(
         Vec3::new(0.0, -15.0, 5.),
         Vec3::new(0.0, 0.0, 0.0),
@@ -57,29 +57,35 @@ pub fn make_scene_with_eight_boxes() -> (Camera, Vec3, Vec<Box<dyn Shape>>) {
         shininess: 0.0,
         specular_coef: 0.0,
     };
-    //let red = Material { color: Vec3::new(1.0, 0.0, 0.0), ambient: 0.3, reflection: 0.0, shininess: 0.0, specular_coef: 0.0 };
+    let red = Material {
+        color: Vec3::new(1.0, 0.0, 0.0),
+        ambient: 0.3,
+        reflection: 0.0,
+        shininess: 0.0,
+        specular_coef: 0.0,
+    };
 
-    let mut shapes: Vec<Box<dyn Shape>> = vec![Box::new(Plane {
+    let mut shapes: Vec<Shape> = vec![Shape::Plane {
         normal: Vec3::new(0.0, 0.0, 1.0),
         d: -2.0,
         material: blue,
-    })];
+    }];
 
     for i in 0..8 {
         let mut matrix = Mat4::from_rotation_z(std::f32::consts::PI / 16.0 * i as f32);
         matrix = Mat4::from_translation(Vec3::new(-7.5 + 2.5 * i as f32, 0.0, 0.0)) * matrix;
         let transform = Transform::new(matrix);
 
-        shapes.push(Box::new(TransformedShape::new(
-            Box::new(UnitBox {}),
+        shapes.push(Shape::TransformedShape {
+            shape: Box::new(Shape::UnitBox { material: red }),
             transform,
-        )));
+        });
     }
     (camera, light, shapes)
 }
 
 #[allow(dead_code)]
-pub fn make_scene_cylinder_plane() -> (Camera, Vec3, Vec<Box<dyn Shape>>) {
+pub fn make_scene_cylinder_plane() -> (Camera, Vec3, Vec<Shape>) {
     let camera = Camera::new(
         Vec3::new(0.0, -5.0, -0.75),
         Vec3::new(0.0, 0.0, 0.0),
@@ -104,18 +110,18 @@ pub fn make_scene_cylinder_plane() -> (Camera, Vec3, Vec<Box<dyn Shape>>) {
         specular_coef: 0.0,
     };
 
-    let shapes: Vec<Box<dyn Shape>> = vec![
-        Box::new(Cone { material: red }),
-        Box::new(Plane {
+    let shapes: Vec<Shape> = vec![
+        Shape::Cone { material: red },
+        Shape::Plane {
             normal: Vec3::new(0.0, 0.0, 1.0),
             d: -2.0,
             material: blue,
-        }),
+        },
     ];
     (camera, light, shapes)
 }
 
-pub fn make_cornell_scene() -> (Camera, Vec3, Vec<Box<dyn Shape>>) {
+pub fn make_cornell_scene() -> (Camera, Vec3, Vec<Shape>) {
     let camera = Camera::new(
         Vec3::new(0.0, -5.0, 0.5),
         Vec3::new(0.0, 0.0, 0.5),
@@ -146,43 +152,43 @@ pub fn make_cornell_scene() -> (Camera, Vec3, Vec<Box<dyn Shape>>) {
         specular_coef: 0.0,
     };
 
-    let shapes: Vec<Box<dyn Shape>> = vec![
-        Box::new(Plane {
+    let shapes: Vec<Shape> = vec![
+        Shape::Plane {
             normal: Vec3::new(0.0, 0.0, 1.0),
             d: 0.0,
             material: white,
-        }),
-        Box::new(Plane {
+        },
+        Shape::Plane {
             normal: Vec3::new(0.0, 0.0, -1.0),
             d: -2.0,
             material: white,
-        }),
-        Box::new(Plane {
+        },
+        Shape::Plane {
             normal: Vec3::new(0.0, -1.0, 0.0),
             d: -3.0,
             material: white,
-        }),
-        Box::new(Plane {
+        },
+        Shape::Plane {
             normal: Vec3::new(1.0, 0.0, 0.0),
             d: -2.0,
             material: red,
-        }),
-        Box::new(Plane {
+        },
+        Shape::Plane {
             normal: Vec3::new(-1.0, 0.0, 0.0),
             d: -2.0,
             material: green,
-        }),
-        Box::new(Sphere {
+        },
+        Shape::Sphere {
             center: Vec3::new(-1.2, 0.2, 0.5),
             radius: 0.5,
             material: white,
-        }),
-        Box::new(Cylinder { material: white }),
+        },
+        Shape::Cylinder { material: white },
     ];
     (camera, light, shapes)
 }
 
-pub fn make_axes_scene() -> (Camera, Vec3, Vec<Box<dyn Shape>>) {
+pub fn make_axes_scene() -> (Camera, Vec3, Vec<Shape>) {
     let camera = Camera::new(
         Vec3::new(1.5, 4.0, 1.35),
         Vec3::new(0.5, 0.0, 0.0),
@@ -225,7 +231,7 @@ pub fn make_axes_scene() -> (Camera, Vec3, Vec<Box<dyn Shape>>) {
     let tip_r = 0.12f32;
     let tip_l = 0.35f32;
 
-    let mut shapes: Vec<Box<dyn Shape>> = Vec::new();
+    let mut shapes: Vec<Shape> = Vec::new();
 
     let s_shaft = Mat4::from_scale(Vec3::new(shaft_r, shaft_r, shaft_l));
     let s_tip = Mat4::from_scale(Vec3::new(tip_r, tip_r, tip_l));
@@ -234,41 +240,41 @@ pub fn make_axes_scene() -> (Camera, Vec3, Vec<Box<dyn Shape>>) {
     // X axis (red)
     let r_x = Mat4::from_rotation_y(std::f32::consts::FRAC_PI_2);
 
-    shapes.push(Box::new(TransformedShape::new(
-        Box::new(Cylinder { material: red }),
-        Transform::new(r_x * s_shaft),
-    )));
-    shapes.push(Box::new(TransformedShape::new(
-        Box::new(Cone { material: red }),
-        Transform::new(r_x * t_tip * s_tip),
-    )));
+    shapes.push(Shape::TransformedShape {
+        shape: Box::new(Shape::Cylinder { material: red }),
+        transform: Transform::new(r_x * s_shaft),
+    });
+    shapes.push(Shape::TransformedShape {
+        shape: Box::new(Shape::Cone { material: red }),
+        transform: Transform::new(r_x * t_tip * s_tip),
+    });
 
     // Y axis (green)
     let r_y = Mat4::from_rotation_x(-std::f32::consts::FRAC_PI_2);
-    shapes.push(Box::new(TransformedShape::new(
-        Box::new(Cylinder { material: green }),
-        Transform::new(r_y * s_shaft),
-    )));
-    shapes.push(Box::new(TransformedShape::new(
-        Box::new(Cone { material: green }),
-        Transform::new(r_y * t_tip * s_tip),
-    )));
+    shapes.push(Shape::TransformedShape {
+        shape: Box::new(Shape::Cylinder { material: green }),
+        transform: Transform::new(r_y * s_shaft),
+    });
+    shapes.push(Shape::TransformedShape {
+        shape: Box::new(Shape::Cone { material: green }),
+        transform: Transform::new(r_y * t_tip * s_tip),
+    });
 
     // Z axis (blue)
-    shapes.push(Box::new(TransformedShape::new(
-        Box::new(Cylinder { material: blue }),
-        Transform::new(s_shaft),
-    )));
-    shapes.push(Box::new(TransformedShape::new(
-        Box::new(Cone { material: blue }),
-        Transform::new(t_tip * s_tip),
-    )));
+    shapes.push(Shape::TransformedShape {
+        shape: Box::new(Shape::Cylinder { material: blue }),
+        transform: Transform::new(s_shaft),
+    });
+    shapes.push(Shape::TransformedShape {
+        shape: Box::new(Shape::Cone { material: blue }),
+        transform: Transform::new(t_tip * s_tip),
+    });
 
-    shapes.push(Box::new(Plane {
+    shapes.push(Shape::Plane {
         normal: Vec3::new(0.0, 0.0, 1.0),
         d: -2.0,
         material: white,
-    }));
+    });
 
     (camera, light, shapes)
 }
--- src/shape/mod.rs
+++ src/shape/mod.rs
@@ -2,26 +2,149 @@ use glam::Vec3;
 
 use crate::types::{Hit, Material, Ray, Transform, Transformable};
 
-pub trait Shape {
-    // Returns (t, normal_at_hit, material) for the first intersection along the ray
-    fn intersect(&self, ray: &Ray) -> Option<Hit>;
+pub enum Shape {
+    UnitBox {
+        material: Material,
+    },
+    Sphere {
+        center: Vec3,
+        radius: f32,
+        material: Material,
+    },
+    Plane {
+        normal: Vec3,
+        d: f32,
+        material: Material,
+    },
+    Cylinder {
+        material: Material,
+    },
+    Cone {
+        material: Material,
+    },
+    TransformedShape {
+        shape: Box<Shape>,
+        transform: Transform,
+    },
 }
 
-pub struct TransformedShape {
-    pub shape: Box<dyn Shape>,
-    pub transform: Transform,
-}
+impl Shape {
+    pub fn intersect(&self, ray: &Ray) -> Option<Hit> {
+        match self {
+            Shape::TransformedShape { shape, transform } => {
+                let transformed_ray = ray.to_local_coordinates(transform);
+                shape
+                    .intersect(&transformed_ray)
+                    .map(|hit| hit.to_global_coordinates(transform))
+            }
+            Shape::UnitBox { material } => {
+                let mut min = f32::MAX;
+                let mut max = f32::MIN;
+                let mut min_pos = 0;
+                let mut max_pos = 0;
 
-impl TransformedShape {
-    pub fn new(shape: Box<dyn Shape>, transform: Transform) -> Self {
-        Self { shape, transform }
-    }
-}
+                for i in 0..3 {
+                    let (t_near, t_far) = if ray.direction[i] < 0.0 {
+                        (
+                            (1.0 - ray.origin[i]) / ray.direction[i],
+                            (-1.0 - ray.origin[i]) / ray.direction[i],
+                        )
+                    } else {
+                        (
+                            (-1.0 - ray.origin[i]) / ray.direction[i],
+                            (1.0 - ray.origin[i]) / ray.direction[i],
+                        )
+                    };
+                    if t_near > max {
+                        max = t_near;
+                        max_pos = i;
+                    }
+                    if t_far < min {
+                        min = t_far;
+                        min_pos = i;
+                    }
+                }
 
-impl Shape for TransformedShape {
-    fn intersect(&self, ray: &Ray) -> Option<Hit> {
-        let transformed_ray = ray.to_local_coordinates(&self.transform);
-        self.shape.intersect(&transformed_ray).map(|hit|hit.to_global_coordinates(&self.transform))
+                if max <= min {
+                    let (t, pos) = if max > 0.0 {
+                        (max, max_pos)
+                    } else {
+                        (min, min_pos)
+                    };
+                    let p = ray.origin + ray.direction * t;
+                    let mut n = Vec3::new(0.0, 0.0, 0.0);
+                    n[pos] = 1.0f32 * p[pos].signum();
+                    Some(Hit::new(t, n, *material))
+                } else {
+                    None
+                }
+            }
+            Shape::Sphere {
+                material,
+                center,
+                radius,
+            } => {
+                let oc = ray.origin - center;
+                let b = ray.direction.dot(oc);
+                let c = oc.dot(oc) - radius * radius;
+                let discriminant = b * b - c;
+                if discriminant < 0.0 {
+                    None
+                } else {
+                    let t = -b - discriminant.sqrt();
+                    let p = ray.origin + ray.direction * t;
+                    let n = (p - center).normalize();
+                    Some(Hit::new(t, n, *material))
+                }
+            }
+            Shape::Plane {
+                normal,
+                d,
+                material,
+            } => {
+                let cos = normal.dot(ray.direction);
+                if cos.abs() < f32::EPSILON {
+                    None
+                } else {
+                    let t = (d - normal.dot(ray.origin)) / cos;
+                    if t < 0.0 {
+                        None
+                    } else {
+                        Some(Hit::new(t, normal.normalize(), *material))
+                    }
+                }
+            }
+            Shape::Cylinder { material } => {
+                let mut best_hit =
+                    intersect_cap_with_radius_one(ray, 1.0, Vec3::new(0.0, 0.0, 1.0), material);
+
+                if let Some(hit_side) =
+                    intersect_cap_with_radius_one(ray, 0.0, Vec3::new(0.0, 0.0, -1.0), material)
+                {
+                    best_hit = best_hit.filter(|hit| hit.t < hit_side.t).or(Some(hit_side))
+                }
+
+                if let Some(hit_side) =
+                    intersect_cylinder_infinite(ray, material).filter(test_if_hits_between_0_1(ray))
+                {
+                    best_hit = best_hit.filter(|hit| hit.t < hit_side.t).or(Some(hit_side))
+                }
+
+                best_hit
+            }
+            Shape::Cone { material } => {
+                let mut best_hit =
+                    intersect_cap_with_radius_one(ray, 0.0, Vec3::new(0.0, 0.0, -1.0), material);
+
+                if let Some(hit_side) =
+                    intersect_cone_infinite(ray, material).filter(test_if_hits_between_0_1(ray))
+                {
+                    best_hit = best_hit.filter(|hit| hit.t < hit_side.t).or(Some(hit_side))
+                }
+
+                best_hit
+            }
+        }
     }
 }
 
@@ -29,12 +152,12 @@ fn intersect_cap_with_radius_one(
     ray: &Ray,
     cap_z_plane: f32,
     hit_normal: Vec3,
-    material: Material,
+    material: &Material,
 ) -> Option<Hit> {
     let t = (cap_z_plane - ray.origin.z) / ray.direction.z;
     let p = ray.origin + ray.direction * t;
     if t > 0.0 && (p.y * p.y + p.x * p.x) < 1.0 {
-        Some(Hit::new(t, hit_normal, material))
+        Some(Hit::new(t, hit_normal, *material))
     } else {
         None
     }
@@ -49,13 +172,13 @@ fn solve_quadratic(a: f32, b: f32, c: f32) -> Option<f32> {
     }
 }
 
-fn intersect_cylinder_infinite(ray: &Ray, material: Material) -> Option<Hit> {
+fn intersect_cylinder_infinite(ray: &Ray, material: &Material) -> Option<Hit> {
     let a = ray.direction.x * ray.direction.x + ray.direction.y * ray.direction.y;
     let b = 2f32 * (ray.direction.x * ray.origin.x + ray.direction.y * ray.origin.y);
     let c = ray.origin.x * ray.origin.x + ray.origin.y * ray.origin.y - 1.0;
     solve_quadratic(a, b, c).map(|t| {
         let p = ray.origin + ray.direction * t;
-        Hit::new(t, Vec3::new(p.x, p.y, 0.0), material)
+        Hit::new(t, Vec3::new(p.x, p.y, 0.0), *material)
     })
 }
 
@@ -75,7 +198,7 @@ fn intersect_cone_infinite_quadratic(ray: &Ray, material: Material) -> Option<Hi
     })
 }
 
-fn intersect_cone_infinite(ray: &Ray, material: Material) -> Option<Hit> {
+fn intersect_cone_infinite(ray: &Ray, material: &Material) -> Option<Hit> {
     let a = ray.direction.x * ray.direction.x + ray.direction.y * ray.direction.y
         - ray.direction.z * ray.direction.z;
     let b = 2f32
@@ -86,7 +209,7 @@ fn intersect_cone_infinite(ray: &Ray, material: Material) -> Option<Hit> {
         - (1.0 - ray.origin.z) * (1.0 - ray.origin.z);
     solve_quadratic(a, b, c).map(|t| {
         let p = ray.origin + ray.direction * t;
-        Hit::new(t, Vec3::new(p.x, p.y, 1.0 - p.z).normalize(), material)
+        Hit::new(t, Vec3::new(p.x, p.y, 1.0 - p.z).normalize(), *material)
     })
 }
 
@@ -94,151 +217,5 @@ fn test_if_hits_between_0_1(ray: &Ray) -> impl Fn(&Hit) -> bool {
     |h| {
         let p = h.point(ray);
         p.z > 0.0 && p.z < 1.0
-    }
-}
-
-pub struct Cone {
-    pub material: Material,
-}
-
-impl Shape for Cone {
-    fn intersect(&self, ray: &Ray) -> Option<Hit> {
-        let mut best_hit =
-            intersect_cap_with_radius_one(ray, 0.0, Vec3::new(0.0, 0.0, -1.0), self.material);
-
-        if let Some(hit_side) =
-            intersect_cone_infinite(ray, self.material).filter(test_if_hits_between_0_1(ray))
-        {
-            best_hit = best_hit.filter(|hit| hit.t < hit_side.t).or(Some(hit_side))
-        }
-
-        best_hit
-    }
-}
-
-pub struct Cylinder {
-    pub material: Material,
-}
-
-impl Shape for Cylinder {
-    fn intersect(&self, ray: &Ray) -> Option<Hit> {
-        let mut best_hit =
-            intersect_cap_with_radius_one(ray, 1.0, Vec3::new(0.0, 0.0, 1.0), self.material);
-
-        if let Some(hit_side) =
-            intersect_cap_with_radius_one(ray, 0.0, Vec3::new(0.0, 0.0, -1.0), self.material)
-        {
-            best_hit = best_hit.filter(|hit| hit.t < hit_side.t).or(Some(hit_side))
-        }
-
-        if let Some(hit_side) =
-            intersect_cylinder_infinite(ray, self.material).filter(test_if_hits_between_0_1(ray))
-        {
-            best_hit = best_hit.filter(|hit| hit.t < hit_side.t).or(Some(hit_side))
-        }
-
-        best_hit
-    }
-}
-
-pub struct UnitBox {}
-
-impl Shape for UnitBox {
-    fn intersect(&self, ray: &Ray) -> Option<Hit> {
-        let mut min = f32::MAX;
-        let mut max = f32::MIN;
-        let mut min_pos = 0;
-        let mut max_pos = 0;
-
-        for i in 0..3 {
-            let (t_near, t_far) = if ray.direction[i] < 0.0 {
-                (
-                    (1.0 - ray.origin[i]) / ray.direction[i],
-                    (-1.0 - ray.origin[i]) / ray.direction[i],
-                )
-            } else {
-                (
-                    (-1.0 - ray.origin[i]) / ray.direction[i],
-                    (1.0 - ray.origin[i]) / ray.direction[i],
-                )
-            };
-            if t_near > max {
-                max = t_near;
-                max_pos = i;
-            }
-            if t_far < min {
-                min = t_far;
-                min_pos = i;
-            }
-        }
-
-        if max <= min {
-            let (t, pos) = if max > 0.0 {
-                (max, max_pos)
-            } else {
-                (min, min_pos)
-            };
-            let p = ray.origin + ray.direction * t;
-            let mut n = Vec3::new(0.0, 0.0, 0.0);
-            n[pos] = 1.0f32 * p[pos].signum();
-            Some(Hit::new(
-                t,
-                n,
-                Material {
-                    color: Vec3::new(1.0, 0.0, 0.0),
-                    ambient: 0.3,
-                    reflection: 0.0,
-                    shininess: 0.0,
-                    specular_coef: 0.0,
-                },
-            ))
-        } else {
-            None
-        }
-    }
-}
-
-pub struct Plane {
-    pub normal: Vec3,
-    pub d: f32,
-    pub material: Material,
-}
-
-impl Shape for Plane {
-    fn intersect(&self, ray: &Ray) -> Option<Hit> {
-        let cos = self.normal.dot(ray.direction);
-        if cos.abs() < f32::EPSILON {
-            None
-        } else {
-            let t = (self.d - self.normal.dot(ray.origin)) / cos;
-            if t < 0.0 {
-                None
-            } else {
-                Some(Hit::new(t, self.normal.normalize(), self.material))
-            }
-        }
-    }
-}
-
-pub struct Sphere {
-    pub center: Vec3,
-    pub radius: f32,
-    pub material: Material,
-}
-
-impl Shape for Sphere {
-    fn intersect(&self, ray: &Ray) -> Option<Hit> {
-        let oc = ray.origin - self.center;
-        let b = ray.direction.dot(oc);
-        let c = oc.dot(oc) - self.radius * self.radius;
-        let discriminant = b * b - c;
-        if discriminant < 0.0 {
-            None
-        } else {
-            let t = -b - discriminant.sqrt();
-            let p = ray.origin + ray.direction * t;
-            let n = (p - self.center).normalize();
-            Some(Hit::new(t, n, self.material))
-        }
     }
 }
